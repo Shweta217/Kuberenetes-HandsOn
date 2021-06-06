48b9a452688d84c5f6827814d1d5aa304778ff74
Ubuntu <ubuntu@ip-10-0-127-248.ap-south-1.compute.internal>
2021-06-06 10:42:49 +0000

    pushing code

---
+++ count3/src/test/java/com/example/count3/Count3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.count3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Count3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
